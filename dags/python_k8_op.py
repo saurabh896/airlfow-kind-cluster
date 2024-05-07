25e111c58e6750d6187abf53450fbbf370373d51
ts-saurabhprak.kadam <ts-saurabhprak.kadam@rakuten.com>
2024-05-07 22:25:35 +0900

    Update python_k8_op.py

--- dags/python_k8_op.py
+++ dags/python_k8_op.py
@@ -24,9 +24,7 @@ annotations = {
                             "vault.hashicorp.com/agent-pre-populate-only": "true",
                             "vault.hashicorp.com/role": "basic-secret-role",
                             "vault.hashicorp.com/agent-inject-secret-helloworld.json": "secret/basic-secret/helloworld",
-                            "vault.hashicorp.com/agent-inject-template-helloworld.json": '''{{ with secret "secret/basic-secret/helloworld" }}
-                                {{ .Data.data | toJSON }}
-                                {{ end }}''', 
+                            "vault.hashicorp.com/agent-inject-template-helloworld.json": '''{{ with secret "secret/basic-secret/helloworld" }}{{ .Data.data | toJSON }}{{ end }}''', 
                             "vault.hashicorp.com/tls-skip-verify": "true",
                           }
 passing = KubernetesPodOperator(namespace='default',
