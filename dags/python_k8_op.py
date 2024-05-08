40a91ed2526e727624390d8415b550048943925a
Saurabh Kadam <saurabhkdm721@gmail.com>
2024-05-08 13:41:58 +0900

    Update python_k8_op.py

--- dags/python_k8_op.py
+++ dags/python_k8_op.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 dag = DAG(
-    'kubernetes_hello_world', default_args=default_args, schedule_interval=timedelta(minutes=10))
+    'kubernetes_hello_world_with_annotations', default_args=default_args, schedule_interval=timedelta(minutes=10))
 
 role = "helloworld"
 start = DummyOperator(task_id='start', dag=dag)
