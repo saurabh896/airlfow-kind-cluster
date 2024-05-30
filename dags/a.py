359cb98f499b55101266e3957f3991341b220cf9
Saurabh Kadam <saurabhkdm721@gmail.com>
2024-05-30 12:34:58 +0900

    Update a.py

--- dags/a.py
+++ dags/a.py
@@ -1,34 +1,18 @@
-import logging 
-from datetime import datetime  
-
-from airflow import DAG 
-from airflow.models import Variable 
-from airflow.hooks.base_hook import BaseHook 
+from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+from airflow.hooks.base_hook import BaseHook
 
 
-def get_secrets(**kwargs):    
-    # Test connections   
+def get_secrets(**kwargs):
     conn = BaseHook.get_connection(kwargs['my_conn_id'])
-    logging.info(
-        f"Password: {conn.password}, Login: {conn.login}, "
-        f"URI: {conn.get_uri()}, Host: {conn.host}"
-    )
+    print(f"Password: {conn.password}, Login: {conn.login}, URI: {conn.get_uri()}, Host: {conn.host}")
 
-    # Test variables     
-    test_var = Variable.get(kwargs['var_name'])
-    logging.info(f'my_var_name: {test_var}')
+with DAG('example_secrets_dags', start_date=datetime(2020, 1, 1), schedule_interval=None) as dag:
 
 
-with DAG(   
-    'test_vault_connection',    
-    start_date=datetime(2020, 1, 1),    
-) as dag:      
-    test_task = PythonOperator(         
-        task_id='test-task',         
-        python_callable=get_secrets,         
-        op_kwargs={
-            'my_conn_id': 'connection_to_test',
-            'var_name': 'my_test_var',
-        },     
+    test_task = PythonOperator(
+        task_id='test-task',
+        python_callable=get_secrets,
+        op_kwargs={'my_conn_id': 'smtp_default'},
     )
